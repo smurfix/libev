8f3c4349034f659baf94aa0fd3baf036d704e6d5
Brent Cook <busterb@gmail.com>
2016-09-19 03:47:09 -0500

    add ignored checks around ignored reads

--- ev.c
+++ ev.c
@@ -2464,7 +2464,7 @@ evpipe_write (EV_P_ EV_ATOMIC_T *flag)
       if (evpipe [0] < 0)
         {
           uint64_t counter = 1;
-          write (evpipe [1], &counter, sizeof (uint64_t));
+          if (write (evpipe [1], &counter, sizeof (uint64_t)) != 1) {}
         }
       else
 #endif
@@ -2476,7 +2476,7 @@ evpipe_write (EV_P_ EV_ATOMIC_T *flag)
           buf.len = 1;
           WSASend (EV_FD_TO_WIN32_HANDLE (evpipe [1]), &buf, 1, &sent, 0, 0, 0);
 #else
-          write (evpipe [1], &(evpipe [1]), 1);
+          if (write (evpipe [1], &(evpipe [1]), 1) != 1) {}
 #endif
         }
 
@@ -2497,7 +2497,7 @@ pipecb (EV_P_ ev_io *iow, int revents)
       if (evpipe [0] < 0)
         {
           uint64_t counter;
-          read (evpipe [1], &counter, sizeof (uint64_t));
+          if (read (evpipe [1], &counter, sizeof (counter)) != sizeof(counter)) {}
         }
       else
 #endif
@@ -2511,7 +2511,7 @@ pipecb (EV_P_ ev_io *iow, int revents)
           buf.len = sizeof (dummy);
           WSARecv (EV_FD_TO_WIN32_HANDLE (evpipe [0]), &buf, 1, &recvd, &flags, 0, 0);
 #else
-          read (evpipe [0], &dummy, sizeof (dummy));
+          if (read (evpipe [0], &dummy, sizeof (dummy)) != sizeof(dummy)) {}
 #endif
         }
     }
